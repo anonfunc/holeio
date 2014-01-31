80063c1404f6e345e65ab4b3348fa8acb31f8bf7
anonfunc <anonfunc@gmail.com>
2014-01-30 22:13:02 -0800

    Start without config

--- holeio/__main__.py
+++ holeio/__main__.py
@@ -4,9 +4,9 @@ import ConfigParser
 from holeio import views  # noqa
 from holeio import watcher, downloader
 
-config = ConfigParser.ConfigParser()
-config.read('holeio.cfg')
-
-watcher.start_watching()
-downloader.start()
+if os.path.isfile('holeio.cfg'):
+  #config = ConfigParser.ConfigParser()
+  #config.read('holeio.cfg')
+  watcher.start_watching()
+  downloader.start()
 run(host='localhost', port=8080, reloader=True, debug=True)
