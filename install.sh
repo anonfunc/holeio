14389797089787b782b7398fbbc7c58519b0909e
anonfunc <anonfunc@gmail.com>
2016-02-13 15:46:19 -0800

    Changing start script to an install script.

--- install.sh
+++ install.sh
@@ -9,4 +9,4 @@ fi
 source venv/bin/activate
 pip install -r requirements.txt
 python setup.py develop
-nohup bottle.py -b 0.0.0.0:8080 holeio.app 2>&1 > holeio.log &
+#nohup bottle.py -b 0.0.0.0:8080 holeio.app 2>&1 > holeio.log &
